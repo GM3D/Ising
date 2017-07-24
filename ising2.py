086b97acccb872337f34b52b9e65b8d0fe3e2da7
GM3D <gmomma940@gmail.com>
2017-07-25 04:00:08 +0900

    Calculate order parameter based on sample average Print paramter values on report

--- ising2.py
+++ ising2.py
@@ -29,30 +29,39 @@ class Ising:
             for j in range(N):
                 s += 'o' if self.spins[i][j] == 1 else '+'
             print(s)
+
+    def adj_sum(self, i, j):
+        left = self.spins[i][j-1] if j >= 1 else -1
+        right = self.spins[i][j+1] if j < N - 1 else -1
+        up = self.spins[i - 1][j] if i >= 1 else -1
+        down = self.spins[i + 1][j] if i < N - 1 else -1
+        return left + right + up + down
         
-    def run(self, n_step=0):
+    def run(self, n_step=0, display=False):
         if not n_step:
             n_step = self.n_step
+            half_step = n_step // 2
+            long_range_order = 0.0
         for step in range(n_step):
             (i, j) = ((step // self.N) % self.N, step % self.N)
-            left = self.spins[i][j-1] if j >= 1 else -1
-            right = self.spins[i][j+1] if j < N - 1 else -1
-            up = self.spins[i - 1][j] if i >= 1 else -1
-            down = self.spins[i + 1][j] if i < N - 1 else -1
-            g =  (left + right + up + down)
-            neg_delta_H = -2 * self.spins[i][j] * (g * J  + B)
+            neg_delta_H = -2 * self.spins[i][j] * (self.adj_sum(i, j) * J  + B)
             r = np.exp(self.beta * neg_delta_H) if neg_delta_H < 0 else 1
             s = np.random.uniform()
             if s < r:
                 self.spins[i][j] *= -1
-            if step % (self.n_step / 10)  == 0:
+            if display and (step % (self.n_step / 10)  == 0):
                 self.display()
                 print("step %d, average = %f" % (step, self.average()))
+            if step >= half_step:
+                long_range_order += self.average()
             self.steps_run = step + 1
+            self.long_range_order = long_range_order / (n_step - half_step)
             
     def report(self):
-        print('run %d step, average = %f' % (self.steps_run, self.average()))
-
+        print('N = %d, n_step = %d, beta = %f, B = %f, J = %f' %
+              (self.N, self.n_step, self.beta, self.B, self.J))
+        print('run %d step, average = %f' %
+              (self.steps_run, self.long_range_order))
 
 if __name__ == '__main__':    
     N = 10
