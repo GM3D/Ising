0b6c7d27cb6fcd9ef6eaa18df3a20598bee8df71
GM3D <gmomma940@gmail.com>
2017-07-26 10:36:41 +0900

    Add display flag to __init__() and run().

--- ising2.py
+++ ising2.py
@@ -1,5 +1,4 @@
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import randint as randint
 
@@ -8,22 +7,22 @@ from numpy.random import randint as randint
 # in symbolic notation. i above is actually 2-dim index (i, j)
 # sum for j is taken for all adjacent neighbors of i
 class Ising:
-    def __init__(self, N, n_step, beta, B, J):
+    def __init__(self, N, n_step, beta, B, J, display=False):
+        self.display = display
         self.N = N
         self.n_step = n_step
         self.steps_run = 0
         self.beta = beta
         self.B = B
         self.J = J
-        np.random.seed(int(time.time()))
+        # np.random.seed(int(time.time()))
         self.spins = randint(2, size=(self.N, self.N), dtype=int)
         self.spins = 2*self.spins - 1
 
-    def average(self):
+    def spacial_avg(self):
         return np.sum(self.spins) / (self.N * self.N)
 
-    def display(self):
-        print('-'*N)
+    def disp_spins(self):
         for i in range(N):
             s = ''
             for j in range(N):
@@ -38,6 +37,8 @@ class Ising:
         return left + right + up + down
         
     def run(self, n_step=0, display=False):
+        if not display:
+            display = self.display
         if not n_step:
             n_step = self.n_step
             half_step = n_step // 2
@@ -50,26 +51,29 @@ class Ising:
             if s < r:
                 self.spins[i][j] *= -1
             if display and (step % (self.n_step / 10)  == 0):
-                self.display()
-                print("step %d, average = %f" % (step, self.average()))
+                print('spins[2][2] = %d, -dH = %f, r = %f, s = %f' %
+                      (self.spins[2][2], neg_delta_H, r, s))
+                self.disp_spins()
+                print("step %d, spacial_avg = %f" % (step, self.spacial_avg()))
             if step >= half_step:
-                long_range_order += self.average()
+                long_range_order += self.spacial_avg()
             self.steps_run = step + 1
-            self.long_range_order = long_range_order / (n_step - half_step)
+        # divide sum by number of steps in the latter half, after the run.
+        self.long_range_order = long_range_order / (n_step - half_step)
             
     def report(self):
         print('N = %d, n_step = %d, beta = %f, B = %f, J = %f' %
               (self.N, self.n_step, self.beta, self.B, self.J))
-        print('run %d step, average = %f' %
+        print('run %d step, sample average = %f' %
               (self.steps_run, self.long_range_order))
 
 if __name__ == '__main__':    
-    N = 10
-    n_step = 10000
-    beta = 2.0
+    N = 50
+    n_step = 1000000
+    beta = 0.8
     B = 0
     J = 1.0
-    ising = Ising(N, n_step, beta, B, J)
+    ising = Ising(N, n_step, beta, B, J, display=True)
     ising.run()
     ising.report()
 
